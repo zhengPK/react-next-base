f680b3dee631b62d1bcc98a6feee4f40abd01e7d
ZhengPK <18300622503@163.com>
2026-03-20 20:37:17 +0800

    feat: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      哈哈
+    </div>
+  );
+}
